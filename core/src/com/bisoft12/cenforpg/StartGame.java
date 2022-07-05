e0f1640cd35ac333aa44fa3a880de7b80757c20e
ajsalasa <asalasa@ucenfotec.ac.cr>
2022-07-04 20:44:42 -0600

    Add merchant screen

--- core/src/com/bisoft12/cenforpg/StartGame.java
+++ core/src/com/bisoft12/cenforpg/StartGame.java
@@ -3,6 +3,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.bisoft12.cenforpg.screen.CityScreen;
 import com.bisoft12.cenforpg.screen.HouseScreen;
+import com.bisoft12.cenforpg.screen.MerchantScreen;
 import com.bisoft12.cenforpg.utils.Render;
 import com.bisoft12.cenforpg.utils.Resources;
 
@@ -11,6 +12,6 @@ public class StartGame extends Game {
 	public void create() {
 		Render._Batch = new SpriteBatch();
 		//Resources.MAIN.setScreen(new CityScreen());
-		Resources.MAIN.setScreen(new HouseScreen());
+		Resources.MAIN.setScreen(new MerchantScreen());
 	}
 }//End of class
---
+++ core/src/com/bisoft12/cenforpg/screen/MerchantScreen.java
@@ -0,0 +1,58 @@
+package com.bisoft12.cenforpg.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.bisoft12.cenforpg.io.Inputs;
+import com.bisoft12.cenforpg.utils.Pantalla;
+import com.bisoft12.cenforpg.utils.Render;
+
+public class MerchantScreen  implements Screen {
+    //Para el jugador
+    private Render render;
+    private Inputs input;
+    private Pantalla screen;
+
+    public MerchantScreen() {
+        input = new Inputs();
+        screen = new Pantalla("maps/map/merchant.tmx");
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(this.input);
+    }
+
+    @Override
+    public void render(float delta) {
+
+        render.clearScreen();
+        screen.update(delta);
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        screen.resize(width, height);    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+        //Limpiamos cuando se salga de la pantalla
+        screen.dispose();
+    }
+
+}
