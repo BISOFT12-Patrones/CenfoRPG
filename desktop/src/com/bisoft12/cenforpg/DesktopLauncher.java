52917ff38e4697328f7a391d024daf3d2108c060
mruizo <mruizo@ucenfotec.ac.cr>
2022-08-07 19:08:50 -0600

    Main restored.

--- desktop/src/com/bisoft12/cenforpg/DesktopLauncher.java
+++ desktop/src/com/bisoft12/cenforpg/DesktopLauncher.java
@@ -5,32 +5,36 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.bisoft12.cenforpg.patterns.Creational.Gestor.FabricaCharacter;
 import com.bisoft12.cenforpg.utils.Resources;
 
-import java.util.Scanner;
-
 public class DesktopLauncher {
-	private static Scanner scan;
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle(Resources.APP_TITLE);
+		config.setTitle("Orden Del Tiempo");
+		config.setResizable(false);
+		config.setWindowedMode(Resources.WIDTH, Resources.HEIGHT);
+		new Lwjgl3Application(Resources.MAIN, config);
+		config.setWindowIcon(Files.FileType.Internal,"icons/icon_game.png");
+		new Lwjgl3Application(new StartGame(), config);
+	}
+}// End of Main
 
+
+
+	/* MAIN Consola para verificar Creacion de Personaje
 	static {
 		scan = new Scanner(System.in);
 	}
 
-
 	public static void main (String[] arg) {
-
-
 		int iOpc = 0;
-
 		do {
 			iOpc = mostrarMenu();
 			System.out.print(FabricaCharacter.processFuntion(iOpc));
 		} while (iOpc !=5 );
-
-
 	}
-
 	private static int mostrarMenu() {
 		String cad="";
-
 		cad=cad+"\n";
 		cad=cad+"Ingrese la opcion correspondiente: \n";
 		cad=cad+"1. Crear un Arquero \n";
@@ -39,10 +43,9 @@ public class DesktopLauncher {
 		cad=cad+"4. Imprimir Codigos existentes\n";
 		cad=cad+"5. Salir \n";
 		cad=cad+"\n";
-
 		System.out.println(cad);
-
 		return scan.nextInt();
-	}
+	} */
 
-}
+
+
