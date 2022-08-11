186953ff7812457e9062e2a4f45ccd1f6f4c97b3
Alberto Solano <albertosolanovillalta@hotmail.com>
2022-08-10 20:24:27 -0600

    Cambios Nela, si estan malos culpa de ella :)

---
+++ core/src/com/bisoft12/cenforpg/patterns/Creational/MetodoFab/Pocion_VitalidadGrupal.java
@@ -0,0 +1,15 @@
+package com.bisoft12.cenforpg.patterns.Creational.MetodoFab;
+
+public class Pocion_VitalidadGrupal extends Pocion{
+
+    // ID = 4
+ public Pocion_VitalidadGrupal() {
+    setNombre("Mana Colectivo");
+ }
+
+    /****************** SECCION: Implementación de las funciones abstractas *******************/
+
+
+
+
+}//
